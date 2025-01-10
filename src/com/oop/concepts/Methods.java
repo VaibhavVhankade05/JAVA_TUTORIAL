daba2cb273bfa97092edf15298178d9a2657d9ca
VaibhavVhankade05 <vaibhavvhankade27@gmail.com>
2025-01-10 11:31:08 +0530

    second commit

---
+++ src/com/oop/concepts/Methods.java
@@ -0,0 +1,87 @@
+package com.oop.concepts;
+
+import java.util.Scanner;
+
+public class Methods 
+{
+	/*
+		Method in Java
+		Method is a way to perform some task.
+		Instructions that performs a specific task
+		A method is a block of code or collection of statements or a set of code grouped together to perform a certain task or operation.
+		It is used to achieve the reusability (write a method once and use it many times.) of code
+
+	 * Public: The method is accessible by all classes when we use public specifier in our application.
+
+	 * Private: When we use a private access specifier, the method is accessible only in the classes in which it is defined.
+
+	 * Protected: When we use protected access specifier, the method is accessible within the same package or subclasses in a different package.
+
+	 * Default: When we do not use any access specifier in the method declaration, Java uses default access specifier by default. It is visible only from the same package only.
+
+		1. without_return_without_parameter 
+		2. without_return_with_parameter
+		3. with_return_without_parameter
+		4. with_return_with_parameter
+	 */
+
+	// 1. without_return_without_parameter 	
+	void add1()
+	{
+		int a,b,c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first Value:");
+		a = sc.nextInt();
+		System.out.println("Enter second value:");
+		b = sc.nextInt();
+		c = a + b;
+		System.out.println("Addition Of :" + c);
+
+	}
+
+
+	
+	// 2. without_return_with_parameter
+	void Add2(int a, int b)
+	{
+		System.out.println("Addition: "+ a+b);
+	}
+	
+	
+	
+	// 3. with_return_without_parameter
+    int Add3()
+    {
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Numbers:");
+        
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = a+b;
+        return c;
+    }
+    
+    
+    
+  // 4. with_return_with_parameter
+    int Add4(int a,int b)
+    {
+        return a+b;
+    }
+    
+    public static void main(String[] args) {
+		
+    	Methods methods = new Methods();
+    	
+    	methods.add1();
+    	
+    	methods.Add2(10, 20);
+    	
+    	int d = methods.Add3();
+    	System.out.println(d);
+    	
+    	int c = methods.Add4(10, 20);
+    	System.out.println(c);
+	}
+}
--- src/com/oop/concepts/OPPConcept.java
+++ src/com/oop/concepts/OPPConcept.java
@@ -3,17 +3,42 @@ package com.oop.concepts;
 public class OPPConcept 
 {
 	/*
-	Object-Oriented Programming (OOP) is a programming paradigm based on the real world Object.
-	Object which contain data and methods (functions) to manipulate the data. 
-	Java is a fully object-oriented programming language, and understanding OOP concepts is crucial for writing efficient and maintainable Java code.
-	
-	
-	# Four Pillars Of Object-Oriented Programming (OOP)
-	1. Encapsulation
-		Binding (or wrapping) code and data together into a single unit are known as encapsulation.
-	
-	2. Inheritance
-	
+		Object-Oriented Programming (OOP) is a programming paradigm based on the real world Object.
+		Object which contain data and methods (functions) to manipulate the data. 
+		Java is a fully object-oriented programming language, and understanding OOP concepts is crucial for writing efficient and maintainable Java code.
+		Object means a real-world entity such as a pen, chair, table, computer, watch, etc.
+		
+			# Object
+				Any entity that has state and behavior is known as an object. For example, a chair, pen, table, keyboard, bike, etc. It can be physical or logical.
+				
+			# Class
+				* Collection of objects is called class. It is a logical entity.
+				* A class can also be defined as a blueprint.
+				
+			# Inheritance
+				* Acquires all the properties and behaviors of a parent object.
+				* Reusability.
+				* Used to achieve runtime polymorphism.
+				
+			# Polymorphism
+				If one task is performed in different ways.
+				
+			# Abstraction
+				Hiding internal implementation and showing functionality.
+				Eg. Calculator
+				
+			# Encapsulation
+				Binding (or wrapping) code and data together into a single unit.
+		
+		# Four Pillars Of Object-Oriented Programming (OOP)
+			1. Encapsulation
+			
+			2. Inheritance
+			
+			3. Polymorphism
+			
+			4. Abstraction
+		
 	
 	*/
 }
